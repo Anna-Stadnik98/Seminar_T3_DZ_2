2ee0e9b058e0451e15209dab984f049dd62ad612
Anna <st.anuta2013@yandex.ru>
2022-12-08 18:34:48 +0300

    Add Seminar_T3_DZ_2_Z10

---
+++ Seminar_T3_DZ_2_Z10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter number :");
+string enterNum = Console.ReadLine();
+int num = Convert.ToInt32(enterNum);
+
+if(num > 99 && num < 1000)
+{
+    int Number = (num / 10) % 10;
+    Console.WriteLine($"Вторая цифра в числе {num} -> {Number}");
+}
+else
+    Console.WriteLine($"Число {num} не трёхзначное ");
